c0db188a898fc7faf6a8c108ec4e595e53f5b733
Ahmed Ramadan <ahmedmedo.am121212@gmail.com>
2023-01-20 10:55:19 +0200

    uploadin new challenge

---
+++ social-media-dashboard-with-theme-switcher-master/social-dashbord/src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div class="attribution">
+        Challenge by
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          rel="noreferrer"
+          target="_blank"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by
+        <a
+          href="https://github.com/Ahmed-AboHmaiD"
+          rel="noreferrer"
+          target="_blank"
+        >
+          Ahmed Ramadan (AboHmaiD)
+        </a>
+        .
+      </div>
+    </div>
+  );
+}
+
+export default App;
